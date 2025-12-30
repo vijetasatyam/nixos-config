3eb30531ec98b1e637489c5ed06627379c7a9e03
vijetasatyam <vijetasatyam@gmail.com>
2025-12-30 18:22:26 +0530

    Update virtual-machines.nix
    Signed-off-by: vijetasatyam <vijetasatyam@gmail.com>

--- modules/dev/virtual-machines.nix
+++ modules/dev/virtual-machines.nix
@@ -1,7 +1,53 @@
-# virtual-machines.nix
-# this config houses all the config related to vms like boxes, virtual box, vmware workstaion, and many other.
-# pkgs.winboat
-# pkgs.gnome-boxes
-#
-#
-# will import containers.nix
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  imports = [ ./containers.nix ];
+
+  options.modules.core.virtualisation = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable KVM/QEMU virtualization and VM tools";
+    };
+  };
+
+  config = lib.mkIf config.modules.core.virtualisation.enable {
+    # --- 1. KVM / Libvirt ---
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_full;
+        runAsRoot = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+
+    # --- 2. Advanced Boot & Hardware ---
+    boot.extraModprobeConfig = "options kvm_intel nested=1";
+    virtualisation.spiceUSBRedirection.enable = true;
+
+    # --- 3. GUI & Tools ---
+    programs.virt-manager.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      quickemu
+      quickgui
+      gnome-boxes
+      freerdp
+      remmina
+      winboat
+    ];
+
+    # --- 4. User Permissions ---
+    users.users.alice.extraGroups = [
+      "libvirtd"
+      "kvm"
+    ];
+  };
+}
