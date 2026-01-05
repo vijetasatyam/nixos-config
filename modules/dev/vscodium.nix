6bcb9b8362739de2c34b0c8e830a93e1cc4a6d6f
vijetasatyam <vijetasatyam@gmail.com>
2026-01-05 09:10:04 +0530

    NixOS Rebuild: Generation

--- modules/dev/vscodium.nix
+++ modules/dev/vscodium.nix
@@ -5,36 +5,62 @@ let
     config = config.nixpkgs.config;
   };
 
-  # Settings tailored for VSCodium
-  vscode-settings = {
+  # Settings for the Isolated VSCodium
+  vscodium-settings = {
     "telemetry.telemetryLevel" = "off";
     "update.mode" = "none";
+    "workbench.settings.enableNaturalLanguageSearch" = false;
     "workbench.accounts.showProfiles" = false;
     "extensions.autoCheckUpdates" = false;
     "extensions.autoUpdate" = false;
     "workbench.startupEditor" = "none";
+    "editor.links" = false;
+    "breadcrumbs.enabled" = false;
     "ai.suggest.enabled" = false;
     "github.copilot.enable" = { "*" = false; };
   };
 
-  settingsFile = pkgs.writeText "vscodium-settings.json" (builtins.toJSON vscode-settings);
+  settingsFile = pkgs.writeText "vscodium-settings.json" (builtins.toJSON vscodium-settings);
 
 in {
   nixpkgs.config.packageOverrides = pkgs: {
-    # Override VSCodium to use the MS Marketplace and strip unwanted features
-    vscode-private = (unstable.vscodium.override {
-      useOfficialExtensions = true;
+    vscodium-isolated = (unstable.vscodium.override {
+      # We remove useOfficialExtensions and use the internal productConfiguration instead
     }).overrideAttrs (oldAttrs: {
       nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
 
-      # Physically remove the authentication and account sync extensions
+      # This manually injects the MS Marketplace URLs into VSCodium's product.json
+      preFixup = (oldAttrs.preFixup or "") + ''
+        gq_path="resources/app/product.json"
+        if [ -f "$out/lib/vscode/$gq_path" ]; then
+          product_json="$out/lib/vscode/$gq_path"
+        else
+          product_json=$(find $out -name product.json | head -n 1)
+        fi
+
+        # Inject Marketplace URLs using python (cleaner than sed for JSON)
+        ${pkgs.python3}/bin/python3 <<EOF
+import json
+with open('$product_json', 'r') as f:
+    data = json.load(f)
+data['extensionsGallery'] = {
+    'serviceUrl': 'https://marketplace.visualstudio.com/_apis/public/gallery',
+    'cacheUrl': 'https://vscode.blob.core.windows.net/gallery/index',
+    'itemUrl': 'https://marketplace.visualstudio.com/items'
+}
+with open('$product_json', 'w') as f:
+    json.dump(data, f, indent=4)
+EOF
+      '';
+
+      # Strip the Sync/Account features physically
       postInstall = (oldAttrs.postInstall or "") + ''
         find $out -name "microsoft-authentication" -type d -exec rm -rf {} +
         find $out -name "github-authentication" -type d -exec rm -rf {} +
         find $out -name "microsoft-account" -type d -exec rm -rf {} +
       '';
 
-      # Wrap the binary with privacy flags and isolated data directory
+      # Wrap binary to use a completely separate config folder
       postFixup = (oldAttrs.postFixup or "") + ''
         wrapProgram $out/bin/codium \
           --add-flags "--disable-telemetry" \
@@ -46,12 +72,12 @@ in {
   };
 
   environment.systemPackages = [
-    pkgs.vscode-private
-    # Alias 'code' to 'codium' so your terminal habits don't have to change
-    (pkgs.writeShellScriptBin "code" "exec codium \"$@\"")
+    pkgs.vscodium-isolated
+    (pkgs.writeShellScriptBin "vscodium" "exec ${pkgs.vscodium-isolated}/bin/codium \"$@\"")
+    (pkgs.writeShellScriptBin "codium-private" "exec ${pkgs.vscodium-isolated}/bin/codium \"$@\"")
   ];
 
-  system.activationScripts.vscode-settings = {
+  system.activationScripts.vscodium-settings = {
     text = ''
       USER_HOME="/home/alice"
       SETTING_DIR="$USER_HOME/.config/VSCodium-Isolated/User"
@@ -62,73 +88,3 @@ in {
     deps = [];
   };
 }
-
-
-
-
-# { config, pkgs, ... }:
-
-# let
-#   unstable = import <unstable> {
-#     config = config.nixpkgs.config;
-#   };
-
-#   # 1. Privacy & UI Settings (Marketplace enabled)
-#   vscode-settings = {
-#     "telemetry.telemetryLevel" = "off";
-#     "update.mode" = "none";
-#     "workbench.settings.enableNaturalLanguageSearch" = false;
-#     "workbench.accounts.showProfiles" = false;
-#     "extensions.autoCheckUpdates" = false;
-#     "extensions.autoUpdate" = false;
-#     "workbench.startupEditor" = "none";
-#     "editor.links" = false;
-#     "breadcrumbs.enabled" = false;
-#     "ai.suggest.enabled" = false;
-#     "github.copilot.enable" = { "*" = false; };
-#   };
-
-#   settingsFile = pkgs.writeText "vscodium-settings.json" (builtins.toJSON vscode-settings);
-
-# in {
-#   nixpkgs.config.packageOverrides = pkgs: {
-#     vscode-private = (unstable.vscodium.override {
-#       # This is the "Magic" part: it tells Codium to use the MS Marketplace
-#       useOfficialExtensions = true;
-#     }).overrideAttrs (oldAttrs: {
-#       nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
-
-#       # 2. Hard-strip any leftover Microsoft/GitHub account logic
-#       postInstall = (oldAttrs.postInstall or "") + ''
-#         find $out -name "microsoft-authentication" -type d -exec rm -rf {} +
-#         find $out -name "github-authentication" -type d -exec rm -rf {} +
-#         find $out -name "microsoft-account" -type d -exec rm -rf {} +
-#       '';
-
-#       postFixup = (oldAttrs.postFixup or "") + ''
-#         wrapProgram $out/bin/codium \
-#           --add-flags "--disable-telemetry" \
-#           --add-flags "--disable-crash-reporter" \
-#           --add-flags "--disable-userdata-auth" \
-#           --add-flags "--user-data-dir ~/.config/VSCodium-Isolated"
-#       '';
-#     });
-#   };
-
-#   # Create an alias so you can still type 'code' in the terminal
-#   environment.systemPackages = [
-#     pkgs.vscode-private
-#     (pkgs.writeShellScriptBin "code" "exec codium \"$@\"")
-#   ];
-
-#   system.activationScripts.vscode-settings = {
-#     text = ''
-#       USER_HOME="/home/alice"
-#       SETTING_DIR="$USER_HOME/.config/VSCodium-Isolated/User"
-#       mkdir -p "$SETTING_DIR"
-#       cp -f ${settingsFile} "$SETTING_DIR/settings.json"
-#       chown -R alice:users "$USER_HOME/.config/VSCodium-Isolated"
-#     '';
-#     deps = [];
-#   };
-# }
