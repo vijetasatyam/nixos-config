9841bece2f66c158a5a250b70f5edca0f74bd314
vijetasatyam <vijetasatyam@gmail.com>
2026-01-18 13:02:41 +0530

    Update system.nix

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -31,19 +31,5 @@
     LC_TELEPHONE = "en_IN.UTF-8";
     LC_TIME = "en_IN.UTF-8";
   };
-  # # Select internationalisation properties.
-  # i18n.defaultLocale = "en_IN";
-
-  # i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "en_IN";
-  #   LC_IDENTIFICATION = "en_IN";
-  #   LC_MEASUREMENT = "en_IN";
-  #   LC_MONETARY = "en_IN";
-  #   LC_NAME = "en_IN";
-  #   LC_NUMERIC = "en_IN";
-  #   LC_PAPER = "en_IN";
-  #   LC_TELEPHONE = "en_IN";
-  #   LC_TIME = "en_IN";
-  # };
 
 }
