998d430b2df17cde2e28dc82bce2679f22fa4945
vijetasatyam <vijetasatyam@gmail.com>
2026-03-27 11:06:28 +0530

    Update system.nix

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -6,46 +6,8 @@
 }:
 {
   # --- Bootloader ---
-  boot.loader.systemd-boot.enable = false; # Explicitly disable the old one
-  boot.loader.efi.canTouchEfiVariables = true;
+  import = [ ./system_bootloader.nix ];
 
-  # Enable rEFInd
-  boot.loader.refind.enable = true;
-
-  boot.loader.refind.extraConfig = ''
-    # --- 1. Force Graphics Mode ---
-    textonly false
-    resolution 1920 1080
-
-    # --- 2. Clean up duplicates & messy files ---
-    dont_scan_dirs EFI/systemd, EFI/BOOT
-    dont_scan_files systemd-bootx64.efi, BOOTX64.EFI
-
-    # --- 3. Clean up the Tool Row ---
-    showtools reboot, shutdown, firmware
-
-    # --- 4. The Theme (Pointing to the Safe Zone) ---
-    icons_dir /EFI/custom-themes/refind-theme-regular/icons/128-48
-    big_icon_size 128
-    small_icon_size 48
-
-    banner /EFI/custom-themes/refind-theme-regular/icons/128-48/bg.png
-    selection_big /EFI/custom-themes/refind-theme-regular/icons/128-48/selection-big.png
-    selection_small /EFI/custom-themes/refind-theme-regular/icons/128-48/selection-small.png
-
-    font /EFI/custom-themes/refind-theme-regular/fonts/source-code-pro-extralight-14.png
-  '';
-
-  # THE FIX: Explicitly disable GRUB to stop the "Failed assertions" error
-  boot.loader.grub.enable = false;
-
-  # Add a timeout so you can see the menu (default is often 0 or 1)
-  boot.loader.timeout = 10;
-  # # Bootloader
-  # # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.enable = false;
-  # boot.loader.refind.enable = true;
   # Timezone (Keeping physical location correct)
   time.timeZone = "Asia/Kolkata";
 
