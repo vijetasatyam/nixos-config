dd16124ee8adf8bb4b6010b76974a2c4d0d821c8
vijetasatyam <vijetasatyam@gmail.com>
2026-09-10 14:14:36 +0530

    NixOS: Gen 49

--- modules/anything_temp/usb.nix
+++ modules/anything_temp/usb.nix
@@ -1,10 +1,13 @@
 {
   # config,
-  # pkgs,
+  pkgs,
   # lib,
   ...
 }: {
   # boot.kernelParams = ["usb-storage.quirks=0781:55a9:u"];
   services.udisks2.enable = true;
   services.gvfs.enable = true;
+  environment.systemPackages = [
+    pkgs.udiskie
+  ];
 }
