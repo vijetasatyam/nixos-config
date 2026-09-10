db2f6d11988c1efb96abeeeb09a1d5e5b005b40b
vijetasatyam <vijetasatyam@gmail.com>
2026-09-10 20:30:17 +0530

    Update flake.nix

--- flake/flake.nix
+++ flake/flake.nix
@@ -50,7 +50,7 @@
     };
 
     # Patch caelestia-shell to fall back to the primary screen under Niri
-    caelestia-shell-patched = caelestia-shell.packages.${system}.default.overrideAttrs (oldAttrs: {
+        caelestia-shell-patched = caelestia-shell.packages.${system}.default.overrideAttrs (oldAttrs: {
           postInstall =
             (oldAttrs.postInstall or "")
             + ''
@@ -79,11 +79,12 @@
                 --replace-fail '    width: win.width - bar.clampedWidth - clampedThickness - win.dragMaskPadding * 2' '    width: win.width - bar.clampedWidth' \
                 --replace-fail '    height: win.height - clampedThickness * 2 - win.dragMaskPadding * 2' '    height: win.height'
 
-              # 4. Dynamically collapse outer border and negative margins on fullscreen
+              # 4. Zero border thickness and perimeter margins on fullscreen
               substituteInPlace $out/share/caelestia-shell/modules/drawers/ContentWindow.qml \
-                --replace-fail 'anchors.margins: -50' 'anchors.margins: root.hasFullscreen ? 0 : -50' \
-                --replace-fail 'borderThickness: root.contentItem.Config.border.thickness' 'borderThickness: root.hasFullscreen ? 0 : root.contentItem.Config.border.thickness' \
-                --replace-fail 'clampedThickness: root.contentItem.Config.border.clampedThickness' 'clampedThickness: root.hasFullscreen ? 0 : root.contentItem.Config.border.clampedThickness'
+                --replace-fail 'readonly property real borderThickness: contentItem.Config.border.thickness * (1 - fsTransitionProg)' \
+                               'readonly property real borderThickness: root.hasFullscreen ? 0 : contentItem.Config.border.thickness * (1 - fsTransitionProg)' \
+                --replace-fail 'anchors.margins: -50 // Make border thicker to smooth out bulge from closed drawers' \
+                               'anchors.margins: root.hasFullscreen ? 0 : -50 // Make border thicker to smooth out bulge from closed drawers'
             '';
         });
 
