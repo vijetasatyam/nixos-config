53c896bc6b741cd3332fac9da775bc4b1340dadd
vijetasatyam <vijetasatyam@gmail.com>
2026-09-11 16:20:23 +0530

    Update toggle-wc.nix

--- modules/home/compositor/toggle-wc.nix
+++ modules/home/compositor/toggle-wc.nix
@@ -64,10 +64,10 @@ in {
   systemd.user.services.inir-autostart = {
     Unit = {
       Description = "Auto-start default shell on Niri";
-      After = [ "niri.service" ];
+      After = ["niri.service"];
     };
     Install = {
-      WantedBy = [ "niri.service" ];
+      WantedBy = ["niri.service"];
     };
     Service = {
       Type = "oneshot";
