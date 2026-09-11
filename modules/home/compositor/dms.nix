947c80703ab93e4cc59df5bc8b9254b8547ed7d6
vijetasatyam <vijetasatyam@gmail.com>
2026-09-11 16:09:57 +0530

    Update dms.nix

--- modules/home/compositor/dms.nix
+++ modules/home/compositor/dms.nix
@@ -4,24 +4,10 @@
   inputs,
   ...
 }: let
-  qsBin = "${inputs.quickshell.packages.${pkgs.system}.default}/bin/quickshell";
-
-  dmsPackage = pkgs.writeShellScriptBin "dms" ''
-    export DMS_SOCKET="''${DMS_SOCKET:-''${XDG_RUNTIME_DIR:-/run/user/$UID}/dms.sock}"
-
-    ENTRY="${inputs.dms}"
-    if [ -d "${inputs.dms}/quickshell" ]; then
-      ENTRY="${inputs.dms}/quickshell"
-    fi
-    exec ${qsBin} -p "$ENTRY" "$@"
-  '';
+  dmsPkg = inputs.dms.packages.${pkgs.system}.default;
 in {
   home.packages = [
     inputs.quickshell.packages.${pkgs.system}.default
-    dmsPackage
+    dmsPkg
   ];
-
-  home.sessionVariables = {
-    DMS_SOCKET = "\${XDG_RUNTIME_DIR}/dms.sock";
-  };
 }
