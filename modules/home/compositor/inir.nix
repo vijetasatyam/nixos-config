acb36fbc591a13ff04e754088cee58ca7588f129
vijetasatyam <vijetasatyam@gmail.com>
2026-09-11 13:12:34 +0530

    Update inir.nix

--- modules/home/compositor/inir.nix
+++ modules/home/compositor/inir.nix
@@ -1,8 +1,22 @@
 #/home/alice/nixos-config/modules/home/compositor/inir.nix
 {
-  # pkgs,
-  # theme,
+  inputs,
+  pkgs,
   ...
 }: {
+  imports = [
+    inputs.inir.homeModules.inir
+  ];
 
+  programs.inir = {
+    enable = true;
+    # Keep compositor auto-start manual or set to "niri"
+    # To run iNiR as the primary active service on session start:
+    service.compositor = "niri";
+    extraPackages = [
+      pkgs.niri
+      pkgs.easyeffects
+    ];
+    configSymlink.enable = true;
+  };
 }
