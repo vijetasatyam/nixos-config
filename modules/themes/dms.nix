1f885373dd4169df2d05b7cb5a270e39948ba76a
vijetasatyam <vijetasatyam@gmail.com>
2026-09-13 15:14:43 +0530

    Update dms.nix

--- modules/themes/dms.nix
+++ modules/themes/dms.nix
@@ -11,15 +11,15 @@
     quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
     systemd = {
-      enable = true;           # Generates and starts dms.service on login
+      enable = true; # Generates and starts dms.service on login
       restartIfChanged = true; # Restarts dms when the package updates
     };
 
     # Core features & widgets
-    enableSystemMonitoring = true;   # dgop backend
-    enableVPN = false;               # Set to true if managing VPN via widget
-    enableDynamicTheming = true;     # Matugen theming
-    enableAudioWavelength = true;    # Cava audio visualizer
-    enableCalendarEvents = true;     # Khal calendar events
+    enableSystemMonitoring = true; # dgop backend
+    enableVPN = false; # Set to true if managing VPN via widget
+    enableDynamicTheming = true; # Matugen theming
+    enableAudioWavelength = true; # Cava audio visualizer
+    enableCalendarEvents = true; # Khal calendar events
   };
 }
