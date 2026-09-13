b3bdb664a0b55ef4355a66a71c93ac822760c768
vijetasatyam <vijetasatyam@gmail.com>
2026-09-13 16:05:28 +0530

    Update niri.nix

--- modules/core/niri.nix
+++ modules/core/niri.nix
@@ -6,6 +6,12 @@
   # Polkit is required for privilege escalation in Wayland (e.g., GUI sudo)
   security.polkit.enable = true;
 
+
+  environment.systemPackages = [
+    pkgs.nirius
+  ];
+
+
   # Use greetd as a lightweight Display Manager
   services.greetd = {
     enable = true;
